c4afc4e97859614a35b83470aaebb58f4e9919fe
Otju <ottoj.soderman@gmail.com>
2024-10-30 17:27:23 +0200

    Test that new actions fail correctly

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,6 @@
 import BoardContainer from "@pages/BoardContainer"
-import Home from "@pages/Home"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from '@pages/Home'
 
 const Router = () => {
   return (
