6c7a76245d7adaa22c79f79331d9d3d2708ed016
Otju <ottoj.soderman@gmail.com>
2024-11-12 13:27:39 +0200

    Fix CORS issue in Azure

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -1,9 +1,10 @@
 import os
 from .settings import *  # noqa: F403
 
-ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
-CORS_ALLOWED_ORIGINS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+allowed_origins = ["https://" + os.environ["FRONTEND_HOSTNAME"]] if "FRONTEND_HOSTNAME" in os.environ else []
+ALLOWED_HOSTS = allowed_origins
+CORS_ALLOWED_ORIGINS = allowed_origins
+CSRF_TRUSTED_ORIGINS = allowed_origins
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = False
