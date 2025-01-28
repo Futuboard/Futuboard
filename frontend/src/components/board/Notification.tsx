58069ffdb34f7dc4525cb76ca95631ab956a5034
Otju <ottoj.soderman@gmail.com>
2025-01-28 15:51:50 +0200

    Make notification not run in background

--- frontend/src/components/board/Notification.tsx
+++ frontend/src/components/board/Notification.tsx
@@ -11,6 +11,7 @@ const Notification = () => {
   const [notificationText, setNotificationText] = useState("")
   const [noticationColor, setNotificationColor] = useState<AlertColor>("info")
   const [notificationTime, setNotificationTime] = useState<number>(0)
+  const [intervalId, setIntervalId] = useState<number | null>(null)
 
   store.subscribe(() => {
     const newText = store.getState().notification.text
@@ -25,20 +26,33 @@ const Notification = () => {
 
   const closeNotification = () => {
     store.dispatch(setNotification({ text: "", type: "info" }))
+    if (intervalId) {
+      clearInterval(intervalId)
+    }
   }
 
   useEffect(() => {
+    // The Mui Alert component transitions with a delay, so the -500 ms makes the notification only dissapear when the bar is visibly at 0%
     if (notificationTime <= -500) {
       closeNotification()
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [notificationTime])
 
   useEffect(() => {
-    const interval = setInterval(() => setNotificationTime((oldTime) => oldTime - 20), 20)
-    return () => {
-      clearInterval(interval)
+    if (!notificationText) {
+      return
     }
-  }, [])
+
+    const newInterValId = setInterval(() => {
+      setNotificationTime((oldTime) => oldTime - 20)
+    }, 20)
+    setIntervalId(newInterValId)
+    return () => {
+      clearInterval(newInterValId)
+    }
+  }, [notificationText])
 
   if (!notificationText) {
     return null
