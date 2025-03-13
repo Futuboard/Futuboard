759b0892465b8584ff2418e567a940dabfb20fe0
Otju <ottoj.soderman@gmail.com>
2025-03-13 15:26:59 +0200

    Add sidebar to charts view

--- frontend/src/pages/Charts.tsx
+++ frontend/src/pages/Charts.tsx
@@ -1,7 +1,8 @@
-import { Box, GlobalStyles } from "@mui/material"
+import { Speed, Water } from "@mui/icons-material"
+import { Box, Divider, GlobalStyles, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
-import { useLocation, useParams } from "react-router-dom"
+import { Link, useLocation, useParams } from "react-router-dom"
 
 import AccessBoardForm from "@/components/board/AccessBoardForm"
 import ToolBar from "@/components/board/Toolbar"
@@ -78,43 +79,76 @@ const Charts: React.FC = () => {
     )
   }
 
-  //         <VelocityChart boardId={id} />
+  let chartName = location.pathname.split("/").pop()
 
-  /*
-          <Box
+  const charts = {
+    cumulativeFlow: {
+      displayName: "Cumulative Flow",
+      component: CumulativeFlowDiagram,
+      Icon: Water
+    },
+    velocity: {
+      displayName: "Velocity",
+      component: VelocityChart,
+      Icon: Speed
+    }
+  }
+
+  if (!charts[chartName as keyof typeof charts]) {
+    chartName = "cumulativeFlow"
+  }
+
+  const ChartComponent = charts[chartName as keyof typeof charts]?.component
+
+  return (
+    <Box sx={{ height: "calc(100vh - 65px)", paddingTop: "65px" }}>
+      <GlobalStyles styles={{ ":root": { backgroundColor: board.background_color || "white" } }} />
+      <ToolBar boardId={id} title={board?.title} chartToolbar={true} />
+      <Box sx={{ position: "relative", height: "100%" }}>
+        <Box
           sx={{
             position: "absolute",
             top: 0,
             left: 0,
             display: "flex",
             flexDirection: "column",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-            width: "300px",
             backgroundColor: "white",
-            height: "100%"
+            height: "100%",
+            borderRight: "2px solid #d1d5db"
           }}
         >
-          {chartTypes.map((chartType) => (
-            <Typography variant="h6">{chartType.name}</Typography>
+          <Box>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              sx={{ marginBottom: 2, paddingTop: 3, widht: "100%", textAlign: "center" }}
+            >
+              Charts
+            </Typography>
+            {
+              <Divider
+                flexItem
+                sx={{ borderBottomWidth: 2, marginX: 2, opacity: chartName === "cumulativeFlow" ? 0 : 1 }}
+              />
+            }
+          </Box>
+          {Object.entries(charts).map(([name, { displayName, Icon }]) => (
+            <Link
+              to={name}
+              key={name}
+              style={{ color: "#213547", backgroundColor: name === chartName ? "#e0e0e2" : "white" }}
+            >
+              <Typography
+                variant="h6"
+                sx={{ display: "flex", alignItems: "center", gap: 0.5, paddingX: 3, paddingY: 2 }}
+                key={name}
+              >
+                <Icon />
+                {displayName}
+              </Typography>
+            </Link>
           ))}
         </Box>
-        */
-
-  const chartName = location.pathname.split("/").pop()
-
-  const charts = {
-    cumulativeFlow: CumulativeFlowDiagram,
-    velocity: VelocityChart
-  }
-
-  const ChartComponent = charts[chartName as keyof typeof charts] || CumulativeFlowDiagram
-
-  return (
-    <Box sx={{ height: "calc(100vh - 65px)", paddingTop: "65px" }}>
-      <GlobalStyles styles={{ ":root": { backgroundColor: board.background_color || "white" } }} />
-      <ToolBar boardId={id} title={`Charts - ${board?.title}`} chartToolbar={true} />
-      <Box sx={{ position: "relative", height: "100%" }}>
         <Box
           sx={{
             display: "flex",
