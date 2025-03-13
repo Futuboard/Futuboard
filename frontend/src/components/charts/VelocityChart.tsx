8f07ccd0e1470075f406d122dffed5c367836797
Otju <ottoj.soderman@gmail.com>
2025-03-13 18:49:04 +0200

    VelocityChart styling

--- frontend/src/components/charts/VelocityChart.tsx
+++ frontend/src/components/charts/VelocityChart.tsx
@@ -1,6 +1,6 @@
 import { CircularProgress, Paper } from "@mui/material"
 import React from "react"
-import { XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Legend, Bar } from "recharts"
+import { XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Legend, Bar, LabelList } from "recharts"
 
 import { useGetVelocityChartDataQuery } from "@/state/apiSlice"
 
@@ -25,16 +25,32 @@ const VelocityChart: React.FC<VelocityChartProps> = ({ boardId }) => {
     return <Paper sx={{ textAlign: "center", typography: "h5", padding: 10 }}>No data</Paper>
   }
 
+  const xAxisLableFormatter = (scopeName: string) => {
+    const maxLength = 15
+    if (scopeName.length > maxLength) {
+      return `${scopeName.substring(0, maxLength)}...`
+    }
+    return scopeName
+  }
+
   return (
     <ChartContainer>
       <BarChart data={data.data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="name" style={{ fontSize: "1.5rem" }} interval={0} tickFormatter={xAxisLableFormatter} />
         <YAxis />
         <Tooltip />
-        <Legend />
-        <Bar dataKey="forecast" fill="#03a9f4" />
-        <Bar dataKey="done" fill="#89c344" />
+        <Legend
+          wrapperStyle={{
+            paddingTop: "1rem"
+          }}
+        />
+        <Bar dataKey="forecast" fill="#03a9f4">
+          <LabelList dataKey="forecast" position="top" style={{ fontSize: "1.5rem" }} />
+        </Bar>
+        <Bar dataKey="done" fill="#89c344">
+          <LabelList dataKey="done" position="top" style={{ fontSize: "1.5rem" }} />
+        </Bar>
       </BarChart>
     </ChartContainer>
   )
