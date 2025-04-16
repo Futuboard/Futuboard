3eaf028663d4e9a239c135fbbe6879e09aedd4ef
Juuso Viskari <viskarj3@aalto.fi>
2025-04-16 15:26:43 +0300

    Add button to remove boards from list

--- frontend/src/components/home/VisitedBoardsList.tsx
+++ frontend/src/components/home/VisitedBoardsList.tsx
@@ -1,14 +1,17 @@
 import SearchIcon from "@mui/icons-material/Search"
-import { Box, List, ListItemButton, Typography, Divider, InputAdornment, TextField } from "@mui/material"
+import CloseIcon from "@mui/icons-material/Close"
+import { Box, List, ListItemButton, Typography, Divider, InputAdornment, TextField, IconButton } from "@mui/material"
 import { Fragment, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 import { getVisitedBoards } from "@/services/utils"
+import { deleteVisitedBoard } from "@/services/utils"
 import { BoardWithOnlyIdAndTitle } from "@/types"
 
 const VisitedBoardList: React.FC = () => {
   const [visitedBoards, setVisitedBoards] = useState<BoardWithOnlyIdAndTitle[]>([])
   const [visibleBoards, setVisibleBoards] = useState<BoardWithOnlyIdAndTitle[]>([])
+  const [filter, setFilter] = useState("")
 
   useEffect(() => {
     setVisitedBoards(getVisitedBoards())
@@ -16,9 +19,16 @@ const VisitedBoardList: React.FC = () => {
   }, [])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setVisibleBoards(
-      visitedBoards.filter((board) => board.title.toUpperCase().includes(event.target.value.toUpperCase()))
-    )
+    const value = event.target.value.toUpperCase()
+    setFilter(value)
+    setVisibleBoards(visitedBoards.filter((board) => board.title.toUpperCase().includes(value)))
+  }
+
+  const handleRemoveFromList = (id: string) => {
+    deleteVisitedBoard({ boardid: id })
+    const updatedBoards = getVisitedBoards()
+    setVisitedBoards(updatedBoards)
+    setVisibleBoards(updatedBoards.filter((board) => board.title.toUpperCase().includes(filter)))
   }
 
   return (
@@ -28,10 +38,7 @@ const VisitedBoardList: React.FC = () => {
           display: "flex",
           flexDirection: "column",
           minWidth: "18em",
-          maxWidth: "25em",
-          borderStyle: "solid",
-          borderWidth: "0px",
-          borderColor: "black"
+          maxWidth: "27em"
         }}
       >
         <Typography variant="h6" color={"black"} marginY={2} sx={{ fontWeight: "bold" }}>
@@ -51,16 +58,28 @@ const VisitedBoardList: React.FC = () => {
           onChange={handleChange}
           autoComplete="off"
         />
-        <List disablePadding sx={{ height: "200px", overflowY: "auto" }}>
+        <List disablePadding sx={{ maxHeight: "235px", overflowY: "auto" }}>
           {visibleBoards.map((board) => (
             <Fragment key={board.boardid}>
               <ListItemButton
                 component={Link}
                 to={"/board/" + board.boardid}
-                sx={{ "&:hover": { color: "#646cff" } }}
+                sx={{
+                  justifyContent: "space-between",
+                  "&:hover": { color: "#646cff" }
+                }}
                 disableRipple
               >
                 <Typography>{board.title}</Typography>
+                <IconButton
+                  size="small"
+                  onClick={(event) => {
+                    event.preventDefault()
+                    handleRemoveFromList(board.boardid)
+                  }}
+                >
+                  <CloseIcon fontSize="small" />
+                </IconButton>
               </ListItemButton>
               <Divider />
             </Fragment>
