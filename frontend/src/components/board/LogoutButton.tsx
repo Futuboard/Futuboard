40286b05930b9c3e8d5b400016665cf9f86fd22d
Otju <ottoj.soderman@gmail.com>
2025-04-22 15:26:15 +0300

    Change terminology

--- frontend/src/components/board/LogoutButton.tsx
+++ frontend/src/components/board/LogoutButton.tsx
@@ -24,15 +24,15 @@ const LogoutButton = ({ boardId }: Props) => {
     <>
       <MenuItem onClick={() => setOpen(true)} sx={{ py: 1 }}>
         <Logout sx={{ fontSize: "1rem", mr: 1 }} />
-        <Typography variant="body2">Log Out</Typography>
+        <Typography variant="body2">Exit board</Typography>
       </MenuItem>
       <Dialog open={open} onClose={() => setOpen(false)}>
         <Box>
-          <DialogTitle>Are you sure you want to log out?</DialogTitle>
+          <DialogTitle>Are you sure you want to exit the board?</DialogTitle>
           <DialogActions>
             <Stack direction="row" spacing={4} justifyContent="flex-end">
               <Button variant="contained" color="primary" onClick={handleLogOut}>
-                Logout
+                Exit
               </Button>
               <Button variant="outlined" color="primary" onClick={() => setOpen(false)}>
                 Cancel
